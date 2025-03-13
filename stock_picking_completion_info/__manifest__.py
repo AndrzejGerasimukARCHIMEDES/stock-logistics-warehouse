57ad2ae12bd59658b17df614ec16f38292c090a1
Rafael Lima <rafael.lima@camptocamp.com>
2025-03-13 16:48:43 +0100

    [IMP] stock_picking_completion_info: pre-commit auto fixes

--- stock_picking_completion_info/__manifest__.py
+++ stock_picking_completion_info/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "16.0.1.0.1",
     "development_status": "Beta",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/wms",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
